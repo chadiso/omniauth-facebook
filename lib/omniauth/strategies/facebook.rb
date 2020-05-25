29c13187286136c8d0a0118e6b71b219099ae781
Roman Osidach <chadiso@users.noreply.github.com>
2020-05-25 12:30:03 +0300

    Dymmy typo fixed

--- lib/omniauth/strategies/facebook.rb
+++ lib/omniauth/strategies/facebook.rb
@@ -24,7 +24,7 @@ module OmniAuth
 
       option :authorization_code_from_signed_request_in_cookie, nil
 
-      option :authorize_options, [:scope, :display, :auth_type :state]
+      option :authorize_options, [:scope, :display, :auth_type, :state]
 
       uid { raw_info['id'] }
 
